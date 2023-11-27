fcafb11e5820edb5e2448d80112082ad72bf764a
Alex Garrett-Smith <alex@codecourse.com>
2023-11-27 16:51:28 +0000

    All code

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <snack />
+  <a href="#" @click.prevent="doSomething">Trigger snack</a>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import Snack from '@/components/Snack'
+
+export default {
+  name: 'App',
+
+  components: {
+    Snack
+  },
+
+  methods: {
+    ...mapActions({
+      snack: 'snack/snack'
+    }),
+
+    doSomething () {
+      this.snack({
+        text: 'This is a snack!',
+        delay: 1000
+      })
+    }
+  }
+}
+</script>
+
+<style src="./assets/styles/app.css" />
